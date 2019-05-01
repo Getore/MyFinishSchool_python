62767f73db961cdf6a5aaa2d1b1a07edb63199ae
shenli <15200831505@163.com>
2019-05-01 09:58:21 +0800

    完成 - 治则部分的文本的准备工作，去除无用信息

--- handleData/main/StartMe.py
+++ handleData/main/StartMe.py
@@ -15,9 +15,9 @@ outputs = open(writeFileName, 'w', encoding='utf-8')    # 设置文本格式
 
 split_file()    # 进行治则治法文件的分隔
 
-for line in inputs:     # line 变量，才是从读取文件的每一行的原始数据
-    line_seg = seg_sentence(line)  # 进行去除停用词、专有名词分词，line_seg 结果是经过 seg_sentence() 方法处理的分词结果，即.../.../..，这样的结果
-    line_seg_latest = synonym_word(line_seg)        # 进行同义词替换（比较耗时）
-    outputs.write(line_seg_latest + '\n')
-outputs.close()
-inputs.close()
+# for line in inputs:     # line 变量，才是从读取文件的每一行的原始数据
+#     line_seg = seg_sentence(line)  # 进行去除停用词、专有名词分词，line_seg 结果是经过 seg_sentence() 方法处理的分词结果，即.../.../..，这样的结果
+#     line_seg_latest = synonym_word(line_seg)        # 进行同义词替换（比较耗时）
+#     outputs.write(line_seg_latest + '\n')
+# outputs.close()
+# inputs.close()
