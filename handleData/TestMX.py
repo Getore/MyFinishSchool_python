3004cf1a482d283f9a98e27eceffc7b48bdd8afc
shenli <15200831505@163.com>
2019-05-01 21:18:17 +0800

    修改 - 正确路径，删除无用代码

--- handleData/TestMX.py
+++ handleData/TestMX.py
@@ -1,9 +1,9 @@
 import jieba
 
 # 分词词典
-jieba.load_userdict("F:\Trainee\pycharm-professional\myTest.txt")
+jieba.load_userdict("F:\\Trainee\\pycharm-professional\\workspace\\handleData\\rules\\suggestFreqTCM.txt")
 
-text = "【中医临床诊疗术语国家标准(治法部分)】NO.1[分类]治则[治法名称]急则治标[注释]与缓则治本相对而言。在大出血、暴泻、剧痛等标症甚急的情况下．应及时救治标病，如止血、止泻、止痛等，然后治其本病的治疗原则。"
+text = "3.3标本兼仁同]治在病证出现标本并重的情况下，可采用治标与治本相结合的治疗原则。"
 
 # 全模式
 seg_list = jieba.cut(text, cut_all=True)
