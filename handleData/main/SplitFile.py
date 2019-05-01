73649b49ab4f591411895f5ad1a467332e1b6028
shenli <15200831505@163.com>
2019-05-01 09:43:22 +0800

    完成 - 按照Java的思路，通过调用另一个文件的方法来处理文件，规范代码

---
+++ handleData/main/SplitFile.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-04-27 11:09:41
+# @Author  : 沈力
+# @Version : v1.0
+# 功能：本页面主要用来将PDF转换得到的TXT文本，分为 治则和治法 两个部分文本
+import re
+
+def split_file():
+    # filePath 表示公共路径
+    filePath = "F:\\Trainee\\pycharm-professional\\workspace\\handleData"
+    # 文件的读取地址
+    readFileName = filePath + "\\words_in\\TreatmentTherapy.txt"
+    # 文件的写地址
+    TREATMENTWRITE = filePath + "\\words_out\\Treatment.txt"
+    THERAPYWRITE = filePath + "\\words_out\\Therapy.txt"
+
+    # 设置文件信息
+    inputs = open(readFileName, 'r', encoding='utf-8')
+    treatmentOutput = open(TREATMENTWRITE, 'w', encoding='utf-8')
+    therapyOutput = open(THERAPYWRITE, 'w', encoding='utf-8')
+
+    flag = 0  # 用来对文本的存储做个开关设置           flag = 1 打开存储开关；   flag = 0 关闭存储开关；
+    mark = 0  # 用来对输出的哪个文件做一个开关设置     mark = 1 输出到治法文件； mark = 0 输出到治则文件；
+    for line in inputs:  # line 变量，才是从读取文件的每一行的原始数据
+        if line == '\n':  # 如果此行只有换行，那么文本将不会输入到输出文件
+            continue
+        if line == '治则\n':  # 当读取到的一行文本是 '治则\n' 时，就打开存储开关
+            flag = 1
+        if line == '附录A\n':
+            flag = 0
+
+        if line == '解表法\n':  # 从 解表法 开始，将文本信息存储在治法文件里
+            mark = 1
+
+        if all((flag == 1, mark == 0)):
+            treatmentOutput.write(re.sub('\n+', '\n', line))  # 使用正则表达式，再一次去除每一行的换行
+        elif all((flag == 1, mark == 1)):
+            therapyOutput.write(re.sub('\n+', '\n', line))
+
+    treatmentOutput.close()
+    therapyOutput.close()
+    inputs.close()
