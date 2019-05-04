9ae64f34914369f124e94af7e63fd3c4db543adb
shenli <15200831505@163.com>
2019-05-04 14:35:50 +0800

    完成 - 治法文本的数据库处理整套流程

--- handleData/main/StartMe.py
+++ handleData/main/StartMe.py
@@ -1,8 +1,11 @@
 from TreatmentInsert import treatment_insert
 from SplitFile import split_file
+from TheraNameInsert import thera_name_insert
 
 # 分成治则、治法两个文件
 split_file()
 # 启动程序，进行 治则数据库的插入
 treatment_insert()
-# theraproject_insert()
+# 启动程序，进行 治法名称数据库的插入
+thera_name_insert()
+
--- handleData/therapy/SQLPrepareTheraproject.py
+++ handleData/therapy/SQLPrepareTheraproject.py
@@ -8,27 +8,28 @@ from string import digits
 from Utils import is_num_word
 import re
 
-# 文件的读取地址
-prepareFile_todo = "F:\\Trainee\\pycharm-professional\\workspace\\handleData\\words_out\\Therapy.txt"
-# 文件的写地址
-prepareFile_done = "F:\\Trainee\\pycharm-professional\\workspace\\handleData\\words_outI\\Therapy.txt"
+def prepare_theraproject():
+    # 文件的读取地址
+    prepareFile_todo = "F:\\Trainee\\pycharm-professional\\workspace\\handleData\\words_out\\Therapy.txt"
+    # 文件的写地址
+    prepareFile_done = "F:\\Trainee\\pycharm-professional\\workspace\\handleData\\words_outI\\Therapy.txt"
 
-inputs = open(prepareFile_todo, 'r', encoding='utf-8')  # 设置文本格式
-outputs = open(prepareFile_done, 'w', encoding='utf-8')  # 设置文本格式
+    inputs = open(prepareFile_todo, 'r', encoding='utf-8')  # 设置文本格式
+    outputs = open(prepareFile_done, 'w', encoding='utf-8')  # 设置文本格式
 
-result = ''
-flag = 0  # 用来判断是否是上一个数字
-content = '' # 用来暂时存储文字
-for line in inputs:  # line 变量，才是从读取文件的每一行的原始数据
-    if is_num_word(line) == 1:
-        continue
+    result = ''
+    flag = 0  # 用来判断是否是上一个数字
+    content = ''  # 用来暂时存储文字
+    for line in inputs:  # line 变量，才是从读取文件的每一行的原始数据
+        if is_num_word(line) == 1:
+            continue
 
-    arrLine = re.split('\n', line)
+        arrLine = re.split('\n', line)
 
-    if arrLine[0][0:1].isdigit():
-        outputs.write('\n' + line)
-    else:
-        outputs.write(arrLine[0])
+        if arrLine[0][0:1].isdigit():
+            outputs.write('\n' + line)
+        else:
+            outputs.write(arrLine[0])
 
-outputs.close()
-inputs.close()
+    outputs.close()
+    inputs.close()
